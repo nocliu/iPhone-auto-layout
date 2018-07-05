c65f442d79337937e5a04b3f3649e44e9064d6d5
Connie Liu <connieliu@lauras-macbook-pro-2.local>
2018-07-04 21:07:30 -0500

    Initial Commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Connie Liu on 7/4/18.
+//  Copyright © 2018 Connie Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
